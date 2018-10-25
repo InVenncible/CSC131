56b305ba90f7582ad074bffd351dab46dddc30ee
andrewtran1 <39430866+andrewtran1@users.noreply.github.com>
2018-10-25 15:12:10 -0700

    Added my information into line 9

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("215489106 - This line was added by Venn Nguyen.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219005892 - This line was added by Andrew Tran.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
